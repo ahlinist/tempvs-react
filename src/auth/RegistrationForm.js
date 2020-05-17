be26e16db3eae1af2e35516715a2e4228fb29261
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-05-17 16:01:04 +0300

    Add registration validation message

--- src/auth/RegistrationForm.js
+++ src/auth/RegistrationForm.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
 
 import { FormattedMessage } from "react-intl";
 
 import { doFetch } from "../util/Fetcher.js";
 
 class RegistrationForm extends Component {
+  constructor() {
+    super();
+    this.state = {messageShown: false, messageText: ''};
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
-    doFetch("/api/user/register", "POST", event);
+
+    const actions = {
+      409: () => this.showMessage("user.alreadyRegistered.message")
+    };
+
+    doFetch("/api/user/register", "POST", event, actions);
   }
+
+  showMessage = (text) => {
+    this.setState({messageShown: true, messageText: text});
+  };
 
   render() {
     return (
@@ -20,6 +34,9 @@ class RegistrationForm extends Component {
           <Form.Label className="col-sm-4"><FormattedMessage id="email.label" defaultMessage="Email"/> *</Form.Label>
           <Form.Control className="col-sm-8" name="email" type="email" required/>
         </Form.Group>
+        {this.state.messageShown &&
+          <FormattedMessage id={this.state.messageText} defaultMessage="User with this email has already been registered"/>
+        }
         <Button variant="secondary" type="submit">
           <FormattedMessage id="request.registration.button" defaultMessage="Request registration"/>
         </Button>
--- src/util/Fetcher.js
+++ src/util/Fetcher.js
@@ -1,6 +1,14 @@
-export const doFetch = (url, method, event) => {
+export const doFetch = (url, method, event, actions) => {
   const data = new FormData(event.target);
   const payload = Object.fromEntries(data);
+
+  const defaultAction = () => alert("Something went wrong!"); //TODO: add i18n
+
+  const responseHandler = (response) => {
+    const status = response.status;
+    const handler = actions[status] || actions.default || defaultAction;
+    handler();
+  };
 
   fetch(url, {
     method: method,
@@ -8,5 +16,5 @@ export const doFetch = (url, method, event) => {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(payload),
-  });
+  }).then(responseHandler);
 };
